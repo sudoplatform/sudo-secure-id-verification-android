3e4d0cf036faeedf0b8f7b7b955d0aeff2aae740
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2023-08-01 17:05:34 +1000

    Release 12.0.1

--- sudoidentityverification/src/main/java/com/sudoplatform/sudoidentityverification/DefaultSudoIdentityVerificationClient.kt
+++ sudoidentityverification/src/main/java/com/sudoplatform/sudoidentityverification/DefaultSudoIdentityVerificationClient.kt
@@ -58,7 +58,7 @@ class DefaultSudoIdentityVerificationClient(
         private const val ERROR_UNSUPPORTED_COUNTRY = "UnsupportedCountryError"
     }
 
-    override val version: String = "12.0.0"
+    override val version: String = "12.0.1"
 
     /**
      * GraphQL client used for calling Sudo service API.
